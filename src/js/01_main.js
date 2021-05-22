0fe70d152b0f3bace4260ffb1e9b77ecd9fb7246
parnasos <lauragraizer@gmail.com>
2021-05-22 17:36:21 +0200

    Render datas

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -11,16 +11,11 @@ function handleSearch(event) {
       const allData = data;
       //todas las pelis encontradas
       console.log(allData);
-      for (const eachData of allData) {
-        const showAllData = eachData.show;
-        console.log(showAllData); // cada una de las pelis dentro del objeto
-        console.log(showAllData.name);
-        console.log(showAllData.image);
-      }
-      // renderFilms(allData);
+
+      renderFilms(allData);
     });
 
-  function filterFilms() {
+  /* function filterFilms() {
     // Coger el valor actual
     const searchText = searchInput.value.toLowerCase();
 
@@ -30,23 +25,39 @@ function handleSearch(event) {
     );
 
     return filteredFilms;
-  }
+  }*/
 
-  const filteredFilms = filterFilms();
+  //const filteredFilms = filterFilms();
 
   // render
-  renderFilms(filteredFilms);
+  // renderFilms(filteredFilms);
 }
 
 function renderFilms(data) {
-  filmsList.innerHTML = "";
-
-  for (const film of allData) {
+  let filmsListContent = filmsList.innerHTML;
+  filmsListContent = "";
+  for (const eachData of data) {
+    const showAllData = eachData.show;
+    //console.log(showAllData); // cada una de las pelis dentro del objeto
+    //console.log(showAllData.name);
+    //console.log(showAllData.image);
+    const newItem = document.createElement("li");
+    const picFilm = document.createElement("img");
+    filmsList.appendChild(newItem);
+    filmsList.appendChild(picFilm);
+    const newContent = document.createTextNode(`${showAllData.name}`);
+    picFilm.setAttribute("src", `${showAllData.image.medium}`);
+    //picFilm.src = `${showAllData.image}`;
+    //const newPic = document.createTextNode(`${showAllData.image}`);
+    newItem.appendChild(newContent);
+    //picFilm.appendChild(picFilm);
+  }
+  /*for (const film of allData) {
     let htmlName = "";
     for (let i = 0; i < film.name.length; i++) {
       htmlName += `<li class="item__film-each" style="background-color:#${film.name[i]};"></li>`;
     }
-  }
+  }*/
 
   /* const paletteFav = favoritePalettes.find((eachPalette) => eachPalette.id === palette.id );
   
