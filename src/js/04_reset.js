f008f4d162b980aa712f058140b2ccffb31d6c18
parnasos <lauragraizer@gmail.com>
2021-05-27 09:49:50 +0200

    Reset btn

--- src/js/04_reset.js
+++ src/js/04_reset.js
@@ -1,6 +1,12 @@
-const btn = document.querySelector(".js-btnReset");
-btn.addEventListener("click", resetFav);
+const btnReset = document.querySelector(".js-btnReset");
+btnReset.addEventListener("click", resetFav);
 
 function resetFav(event) {
-  event.preventDefault();
+  // event.preventDefault();
+  //vaciamos el array de favoritos
+  favoriteFilms = [];
+  //limpiamos el ls
+  localStorage.clear();
+  //recargar la página después de borrar
+  // location.reload();
 }
